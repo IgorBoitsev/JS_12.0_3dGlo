9759e9162a24b348cac1d3a797979bba990a120a
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-29 09:18:21 +0300

    validator

--- plugins/validator/validator.js
+++ plugins/validator/validator.js
@@ -1,21 +1,37 @@
 class Validator {
   constructor({selector, pattern, method}) {
-    this.selector = selector;
+    this.form = document.querySelector(selector);
     this.pattern = pattern;
     this.method = method;
   }
 
   init() {
     this.applyStyle();
+
+    const
   }
 
+  // Обнаружение ошибки в поле ввода данных
   showError(elem) {
-
+    // Добавление класса полю с ошибкой
+    elem.classList.remvoe(`success`);
+    elem.classList.add(`error`);
+    // Сообщение об ошибке
+    const errorDiv = document.createElement(`div`);
+    errorDiv.textContent = `Ошибка в этом поле`;
+    errorDiv.classList.add(`.validator-error`);
+    elem.insertAdjasentElement(`afterend`, errorDiv);
   }
 
+  // 
   showSuccess(elem) {
-
-  }
+    // Добавление класса полю с ошибкой 
+    elem.classList.remvoe(`error`);
+    elem.classList.add(`success`);
+    // Удаление сообщения об ошибке, если таковое осталось
+    if (elem.nextElementSibling.classList.contains(`.validator-error`)) {
+      elem.nextElementSibling.remvoe();
+    }
 
   applyStyle() {
     const style = document.createElement(`style`);
@@ -24,6 +40,10 @@ class Validator {
                          }
                          input.error {
                            outline: 2x solid red;
+                         }
+                         validator-error {
+                           font-size: 14px;
+                           dolor: red;
                          }`
   }
 }
