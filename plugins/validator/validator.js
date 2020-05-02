0ac3bb62526c1570db6ad1c9660da42632e7ffa5
Igor Boitsev <igor.boitsev@gmail.com>
2020-05-02 10:53:05 +0300

    validator

--- plugins/validator/validator.js
+++ plugins/validator/validator.js
@@ -22,7 +22,29 @@ class Validator {
   }
 
   isValid(elem) {
-    return false;
+    const validatorMethod = {
+      notEmpty(elem) {
+        if (elem.value.trim() === '') return false;
+        return true;
+      },
+      pattern(elem, pattern) {
+        console.log(pattern);
+        console.log(elem.value);
+        
+        console.log(pattern.test(elem.value));
+        
+        return pattern.test(elem.value);
+      }
+    };
+
+    const method = this.method[elem.name];
+
+    if (method) {
+      return method.every(item => validatorMethod[item[0]](elem, this.pattern[item[1]]))
+    }
+    // console.log(method);
+    
+    return true;
   }
 
   // 
@@ -38,8 +60,6 @@ class Validator {
 
   // Обнаружение ошибок валидации
   showError(elem) {
-    console.log(elem.nextElementSibling);
-    
     elem.classList.remove('success');
     elem.classList.add('error');
     
@@ -84,14 +104,12 @@ class Validator {
   // Установка шаблонов
   setPattern() {
     if (!this.pattern.user_name)
-      this.pattern.user_name = /^\w$/;
+      this.pattern.user_name = /^\W+$/;
     
     if (!this.pattern.user_phone)  
-      this.pattern.user_phone = /^\+?[78]([-()])*\d{10}$/;
+      this.pattern.user_phone = /^\+?[78]([-()]*\d){10}$/;
 
     if (!this.pattern.user_email)
-      this.pattern.user_email = /^\w+@\w+\.{2,}$/;
-
-    console.log(this.pattern);
+      this.pattern.user_email = /^\w+\.?\w+?@\w+\.\w{2,}$/;
   }
 }
